ef61abaa01fa1e3915dd99159f6cbb61efab56b3
Jay Patel <itsjay0730@gmail.com>
2026-03-01 03:44:34 -0600

    updates

--- ios/CATrack/CATrack/Services/APIService.swift
+++ ios/CATrack/CATrack/Services/APIService.swift
@@ -4,7 +4,7 @@ import Foundation
 class APIService {
     static let shared = APIService()
 //    private var baseURL: String = "http://192.168.10.201:8000"
-    private var baseURL: String = "http://127.0.0.1:8000"
+    private var baseURL: String = "https://unannihilated-tuberculoid-zainab.ngrok-free.dev/docs"
 
     private init() {}
 
